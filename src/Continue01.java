3f98952fc2d1d7724c1a79a40c910257747d655e
TomomiTsukazaki <122886012+tsukazaki-t@users.noreply.github.com>
2023-02-12 22:52:12 +0900

    課題提出

---
+++ src/Continue01.java
@@ -0,0 +1,19 @@
+
+public class Continue01 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		for(int count = 0; count < 5; count++ ) {
+			
+			if(count == 2) {
+				continue;
+			}
+			
+			System.out.println("Continue01 = " + count);
+			
+		}
+
+	}
+
+}
